87fa0a341db9744c89238057168d7f07123df434
Adiaworks <lyuwen0121@gmail.com>
2022-03-20 22:26:50 +1000

    Initial Commit

---
+++ Checklist/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by Wen Lyu on 20/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
