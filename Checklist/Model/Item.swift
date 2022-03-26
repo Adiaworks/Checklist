ef8edb5f683bcead8697e3d9516a58ee2cd1bdb3
Adiaworks <lyuwen0121@gmail.com>
2022-03-26 22:11:33 +1000

    Add ItemModel and ItemViewModel

---
+++ Checklist/Model/Item.swift
@@ -0,0 +1,10 @@
+//
+//  Item.swift
+//  Checklist
+//
+//  Created by Wen Lyu on 26/3/2022.
+//
+
+struct Item {
+    var title: String
+}
---
+++ Checklist/ViewModel/ItemViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ItemViewModel.swift
+//  Checklist
+//
+//  Created by Wen Lyu on 26/3/2022.
+//
+struct ItemViewModel {
+    var item: Item
+    
+    var itemTitile: String {
+        item.title
+    }
+}
