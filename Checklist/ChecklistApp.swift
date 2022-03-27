08696364880fb33b63f8f0ac03a56d5771464f25
Adiaworks <lyuwen0121@gmail.com>
2022-03-27 19:58:04 +1000

    Add more viewModel to be displayed

--- Checklist/ChecklistApp.swift
+++ Checklist/ChecklistApp.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 @main
 struct ChecklistApp: App {
-    var viewModel = [ItemViewModel(
-        item: Item(title: "Watermelon"))]
+    var viewModel = [
+        ItemViewModel(item: Item(title: "Watermelon")),
+        ItemViewModel(item: Item(title: "Peach")),
+        ItemViewModel(item: Item(title: "Rockmelon")),
+        ItemViewModel(item: Item(title: "Strawberry")),
+        ItemViewModel(item: Item(title: "Apple")),
+    ]
     
     var body: some Scene {
         WindowGroup {
