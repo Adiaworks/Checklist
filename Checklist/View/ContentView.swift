dae6d4208b45a1d56d26cd2e3866dc3ef8ce8619
Adiaworks <lyuwen0121@gmail.com>
2022-03-27 19:57:20 +1000

    Use identifiable model data

--- Checklist/View/ContentView.swift
+++ Checklist/View/ContentView.swift
@@ -11,15 +11,26 @@ struct ContentView: View {
     var itemList: [ItemViewModel]
     
     var body: some View {
-        Text(itemList[0].itemTitle)
+        VStack {
+            Text("Checklist").bold()
+        
+        List(itemList) { item in
+            Text(item.itemTitle)
             .padding()
+        }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(itemList: [ItemViewModel(item:
-            Item(title: "Watermelon"))]
-        )
+        ContentView(itemList: [
+           ItemViewModel(item: Item(title: "Watermelon")),
+           ItemViewModel(item: Item(title: "Peach")),
+           ItemViewModel(item: Item(title: "Rockmelon")),
+           ItemViewModel(item: Item(title: "Strawberry")),
+           ItemViewModel(item: Item(title: "Apple")),
+        
+        ])
     }
 }
